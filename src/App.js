493b70d0142e86400f73f4198b62be1a04096872
ADAM ANDREASON <adamandreason@gmail.com>
2021-03-16 01:56:26 -0600

    initial react setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Weather from './components/Weather'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Weather Appiness</h1>
+   <p>Yo.</p>
+   <Weather/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Weather.js
@@ -0,0 +1,8 @@
+
+
+export default function Weather() {
+
+    return(
+        <h2>Your Weather:</h2>
+    )
+}
